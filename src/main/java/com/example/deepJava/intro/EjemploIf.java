2567b43a3490b8438c306381d45d0a4a1942e617
Gomez Ruiz <juan.diego.gomez.ruiz.ext@ing.com>
2022-05-25 12:47:38 +0200

    if

---
+++ src/main/java/com/example/deepJava/intro/EjemploIf.java
@@ -0,0 +1,13 @@
+package com.example.deepJava.intro;
+
+public class EjemploIf {
+    public static void main(String[] args) {
+       int SRM = 10;
+
+       if (SRM >= 9) {
+           System.out.println("Felicidades, has aprobado el SRM");
+       } else {
+           System.out.println("Lo sentimos, has reprobado el SRM");
+       }
+    }
+}

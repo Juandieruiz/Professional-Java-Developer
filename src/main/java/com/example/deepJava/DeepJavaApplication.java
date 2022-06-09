22a49d2aab9a6a3f2039f7192386fead8931082e
Gomez Ruiz <juan.diego.gomez.ruiz.ext@ing.com>
2022-06-09 17:08:48 +0200

    first commit

---
+++ src/main/java/com/example/deepJava/DeepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deepJava/DeepJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

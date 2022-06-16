d1f5c7d28b76dc32dd99a2f5ba0d340c8e792a1c
Diego <juandieruiz6@gmail.com>
2022-06-16 17:33:42 +0200

    arrays

---
+++ src/main/java/com/example/deepJava/arrays/TestArrays.java
@@ -0,0 +1,14 @@
+package com.example.deepJava.arrays;
+
+public class TestArrays {
+    public static void main(String[] args) {
+        int array[] = new int[3];
+        array[0] = 03;
+        array[1] = 20;
+        array[2] = 2000;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}

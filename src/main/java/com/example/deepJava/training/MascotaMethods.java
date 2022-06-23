c01f3cb2ef8188b810a09a01aba44f2de799b44a
Diego <juandieruiz6@gmail.com>
2022-06-23 11:53:02 +0200

    instance of - Herencia & Polimorfismo

--- src/main/java/com/example/deepJava/training/MascotaMethods.java
+++ src/main/java/com/example/deepJava/training/MascotaMethods.java
@@ -2,7 +2,7 @@ package com.example.deepJava.training;
 
 public class MascotaMethods {
 
-    class Animal{
+    public class Animal{
         String name;
         public void breath(){
             System.out.println("I'm Animal and I breath");
@@ -20,11 +20,24 @@ public class MascotaMethods {
 
     public class ExampleHierarchy {
         public void main(String[] args) {
+            Animal a= new Animal();
             Dog dog = new Dog();
             Cat cat = new Cat();
+
+
+            System.out.println(a instanceof Animal); //true
+            System.out.println(a instanceof Dog); //false
+            System.out.println(a instanceof Cat); //false
+
+
             dog.name = "Fido";
             dog.breath();
+            System.out.println(dog instanceof Animal); //true
+
+
             cat.breath();
+            System.out.println(cat instanceof Animal); //true
+
         }
     }
 }

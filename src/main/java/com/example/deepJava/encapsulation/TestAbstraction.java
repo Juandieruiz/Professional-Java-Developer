b7c9cb8bfc930b69688a0b656af8a7e1465038c3
Diego <juandieruiz6@gmail.com>
2022-06-27 10:54:44 +0200

    abstraction methods

---
+++ src/main/java/com/example/deepJava/encapsulation/TestAbstraction.java
@@ -0,0 +1,10 @@
+package com.example.deepJava.encapsulation;
+
+public class TestAbstraction {
+    public static void main(String[] args) {
+        String s = new String();
+        // public method with documentation
+        s.toUpperCase();
+
+    }
+}

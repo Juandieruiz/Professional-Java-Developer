b83eaa2e380f8d10a9291d256965d1397995e4a3
Diego <juandieruiz6@gmail.com>
2022-07-04 17:38:05 +0200

    autoboxing & autounboxing

---
+++ src/main/java/com/example/deepJava/wrapper/AutoBoxingAutoUnboxingTest.java
@@ -0,0 +1,16 @@
+package com.example.deepJava.wrapper;
+
+public class AutoBoxingAutoUnboxingTest {
+    /**
+     *  Autoboxing is a process of converting primitive types to their wrapper classes.
+     *  For example, the primitive type int is converted to the wrapper class Integer.
+     */
+    Integer x = 10;
+
+    /**
+     * Autounboxing is a process of converting wrapper classes to their primitive types.
+     * For example, the wrapper class Integer is converted to the primitive type int.
+     */
+     int y = x;
+
+}

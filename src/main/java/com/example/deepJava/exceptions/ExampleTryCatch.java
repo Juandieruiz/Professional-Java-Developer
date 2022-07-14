4146dcaae84c587c6a7f7215a5c89fe027d4d5aa
Diego <juandieruiz6@gmail.com>
2022-07-14 13:27:10 +0200

    polimorfismo y excepciones

--- src/main/java/com/example/deepJava/exceptions/ExampleTryCatch.java
+++ src/main/java/com/example/deepJava/exceptions/ExampleTryCatch.java
@@ -11,12 +11,18 @@ public class ExampleTryCatch {
             System.out.println("Caculando operación = " + y);
             String name = "juandieruiz";
             System.out.println("Hola " + name.toString());
+            String arr[] = {"comoserprogramador.com","juandieruiz"};
+            System.out.println(arr[3]);
 
-        } catch (ArithmeticException e) {
+        //  Posibles Errores:
+           } catch (ArithmeticException e) {
             System.err.printf("Arithmetic Exception %s \n", e.getMessage());
 
         } catch (NullPointerException e) { // <-- Uso de multiples catchs
             System.err.printf("NullPointer Exception %s \n", e.getMessage());
+         // Si pasan los errores anteriores, se ejecuta este catch
+        } catch (Exception e) {
+            System.err.printf("Exception %s \n", e.getMessage());
         } finally {
             System.out.println("Siempre se ejecuta el finally haya o no una excepción, error o no");
         }

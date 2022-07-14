c9878fa83798f3df3e1c5933f18a858b9f983473
Diego <juandieruiz6@gmail.com>
2022-07-14 13:08:16 +0200

    multiples catchs

--- src/main/java/com/example/deepJava/exceptions/ExampleTryCatch.java
+++ src/main/java/com/example/deepJava/exceptions/ExampleTryCatch.java
@@ -5,15 +5,20 @@ public class ExampleTryCatch {
 
     public static void main(String[] args) {
         try {
-            // int x = 1;
-            int x = 0;
+            // int x = 0;
+            int x = 10;
             int y = 10 / x;
             System.out.println("Caculando operación = " + y);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+            String name = "juandieruiz";
+            System.out.println("Hola " + name.toString());
+
+        } catch (ArithmeticException e) {
+            System.err.printf("Arithmetic Exception %s \n", e.getMessage());
+
+        } catch (NullPointerException e) { // <-- Uso de multiples catchs
+            System.err.printf("NullPointer Exception %s \n", e.getMessage());
         } finally {
-            System.out.println("Siempre se ejecuta el finally haya o no una excepción");
+            System.out.println("Siempre se ejecuta el finally haya o no una excepción, error o no");
         }
     }
 }

710050468cc12baf4dc352bb79de6d16f6d73830
Diego <juandieruiz6@gmail.com>
2022-09-05 12:32:24 +0200

    objects

---
+++ src/main/java/com/example/deepJava/objects/WritingObjects.java
@@ -0,0 +1,6 @@
+package com.example.deepJava.objects;
+
+public class WritingObjects {
+    Developer devs[] = {new Developer("Jhon", "Ruby"), new Developer("James","Java"),
+            new Developer("Moure","Swift")};
+}

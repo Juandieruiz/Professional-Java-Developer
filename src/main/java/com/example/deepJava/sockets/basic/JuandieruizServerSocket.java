0a96b268662dee586fadd5fe28d486b998198311
Diego <juandieruiz6@gmail.com>
2022-09-12 12:01:22 +0200

    ServerSocket Basic I

---
+++ src/main/java/com/example/deepJava/sockets/basic/JuandieruizServerSocket.java
@@ -0,0 +1,29 @@
+package com.example.deepJava.sockets.basic;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class JuandieruizServerSocket {
+    public static void main(String[] args) throws IOException {
+
+        try (ServerSocket socket = new ServerSocket(8090)) {
+
+            System.out.println("Socket Bancario a la espera de peticiones ...");
+            Socket client = socket.accept();
+            System.out.printf("Cliente %s conectado \n", client.getInetAddress().getHostName());
+            DataInputStream dis = new DataInputStream(client.getInputStream());
+            String message = null;
+            do {
+                message = dis.readUTF();
+                System.out.printf("%s dice : %s",client.getInetAddress().getHostName(), message);
+            } while (!"Finish".equals(message));
+            System.out.println("Conexión finalizada.");
+            socket.close();
+            client.close();
+            dis.close();
+        }
+    }
+
+}

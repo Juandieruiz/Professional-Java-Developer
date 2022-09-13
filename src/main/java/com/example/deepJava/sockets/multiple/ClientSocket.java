c81e6a42a9aa54c5888076716920849e370540f8
Diego <juandieruiz6@gmail.com>
2022-09-13 15:56:13 +0200

    client socket

---
+++ src/main/java/com/example/deepJava/sockets/multiple/ClientSocket.java
@@ -0,0 +1,28 @@
+package com.example.deepJava.sockets.multiple;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
+
+public class ClientSocket {
+
+    public static String readMessage() throws IOException {
+        System.out.printf("\n->");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        try(Socket socket = new Socket("localhost",8090);
+            DataOutputStream test = new DataOutputStream(socket.getOutputStream())) {
+            String message = null;
+            do {
+                message = readMessage();
+                test.writeUTF(message);
+            } while (!"salir".equals(message));
+        }
+    }
+}
